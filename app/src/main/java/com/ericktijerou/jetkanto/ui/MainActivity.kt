849e454fddff2ffc810eee3875c35ea35d11e150
Erick Tijero <ericktijerou@gmail.com>
2021-03-24 13:37:37 -0500

    Config application

--- app/src/main/java/com/ericktijerou/jetkanto/ui/MainActivity.kt
+++ app/src/main/java/com/ericktijerou/jetkanto/ui/MainActivity.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.ericktijerou.jetkanto
+package com.ericktijerou.jetkanto.ui
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
